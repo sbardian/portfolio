b1f8ff4faae5acc337d7883b88acadeccacf5fb3
Brian Andrews <sbardian@gmail.com>
2019-03-07 21:26:59 -0700

    Add gatsby-plugin-web-font-loader

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,14 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: "gatsby-plugin-web-font-loader",
+      options: {
+        google: {
+          families: ["Source Sans Pro"],
+        },
+      },
+    },
     `gatsby-plugin-emotion`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
