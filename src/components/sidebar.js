846b8cc2fd026e017567b0b9e793ea25ede3d199
Brian Andrews <sbardian@gmail.com>
2019-03-07 21:28:03 -0700

    Add icon links to sidebar

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -2,11 +2,14 @@
 // eslint-disable-next-line
 import React from "react"
 import { jsx, css } from "@emotion/core"
+import { Link } from "gatsby"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faGithub, faTwitter } from "@fortawesome/free-brands-svg-icons"
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons"
 import sidebarBg from "../images/sidebar-bg.jpg"
 import avatar from "../images/avatar.jpg"
 
 export default () => {
-  console.log("sidebarBg = ", sidebarBg)
   return (
     <div
       css={css`
@@ -44,7 +47,7 @@ export default () => {
         css={css`
           display: grid;
           grid-gap: 20px;
-          grid-template-rows: 60px 200px;
+          grid-template-rows: 60px 150px 50px;
           color: #474444;
         `}
       >
@@ -58,6 +61,38 @@ export default () => {
             Nisi expedita amet in aspernatur qui veritatis doloremque non, ipsa
             iusto, nemo quia.
           </span>
+        </div>
+        <div
+          css={css`
+            display: grid;
+            grid-gap: 20px;
+            grid-template-columns: 20px 20px 20px 20px;
+          `}
+        >
+          <a
+            css={css`
+              color: #666;
+            `}
+            href="https://github.com/sbardian"
+          >
+            <FontAwesomeIcon size="2x" icon={faGithub} />
+          </a>
+          <a
+            css={css`
+              color: #666;
+            `}
+            href="https://twitter.com/xsbardianx"
+          >
+            <FontAwesomeIcon size="2x" icon={faTwitter} />
+          </a>
+          <a
+            css={css`
+              color: #666;
+            `}
+            href="mailto:sbardian@gmail.com?Subject=Dear Developer..."
+          >
+            <FontAwesomeIcon size="2x" icon={faEnvelope} />
+          </a>
         </div>
       </div>
     </div>
