2a21485060f591a830710baf78f36f1a75bafa37
Brian Andrews <sbardian@gmail.com>
2019-03-11 14:17:15 -0600

    Create nav button component

---
+++ src/components/nav-button.js
@@ -0,0 +1,51 @@
+/** @jsx jsx */
+// eslint-disable-next-line
+import React from "react"
+import { Link } from "gatsby"
+import { jsx, css } from "@emotion/core"
+import mq from "./mediaQueries"
+
+export default ({ title, to }) => {
+  return (
+    <div
+      css={css`
+        ${mq.sm(css`
+          width: 100%;
+          max-width: -moz-available;
+        `)}
+      `}
+    >
+      <Link
+        css={css`
+          appearance: none;
+          text-decoration: none;
+          transition: all 0.2s ease-in-out;
+          background-color: transparent;
+          border-radius: 0.35em;
+          border: 3px solid #e1e1e1;
+          color: #e1e1e1;
+          cursor: pointer;
+          display: inline-block;
+          font-weight: 400;
+          line-height: 2.75em;
+          min-width: 7em;
+          max-width: 10em;
+          padding: 0 1.5em;
+          &:hover {
+            background: #2f2d2d;
+            color: #92e5f3;
+            border-color: #92e5f3;
+          }
+          ${mq.sm(css`
+            width: 100%;
+            max-width: -moz-available;
+            max-width: -webkit-fill-available;
+          `)}
+        `}
+        to={to}
+      >
+        {title}
+      </Link>
+    </div>
+  )
+}
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,9 +1,8 @@
 /** @jsx jsx */
 // eslint-disable-next-line
 import React from "react"
-import { Link } from "gatsby"
 import { jsx, css } from "@emotion/core"
-import { Button } from "./styled/button"
+import NavButton from "./nav-button"
 import mq from "./mediaQueries"
 
 export default ({ children }) => {
@@ -22,126 +21,9 @@ export default ({ children }) => {
         `)}
       `}
     >
-      <div
-        css={css`
-          ${mq.sm(css`
-            width: 100%;
-            max-width: -moz-available;
-          `)}
-        `}
-      >
-        <Link
-          css={css`
-            appearance: none;
-            text-decoration: none;
-            transition: all 0.2s ease-in-out;
-            background-color: transparent;
-            border-radius: 0.35em;
-            border: 3px solid #e1e1e1;
-            color: #e1e1e1;
-            cursor: pointer;
-            display: inline-block;
-            font-weight: 400;
-            line-height: 2.75em;
-            min-width: 7em;
-            max-width: 10em;
-            padding: 0 1.5em;
-            &:hover {
-              background: #2f2d2d;
-              color: #92e5f3;
-              border-color: #92e5f3;
-            }
-            ${mq.sm(css`
-              width: 100%;
-              max-width: -moz-available;
-              max-width: -webkit-fill-available;
-            `)}
-          `}
-          to="/"
-        >
-          Home
-        </Link>
-      </div>
-      <div
-        css={css`
-          ${mq.sm(css`
-            width: 100%;
-            max-width: -moz-available;
-          `)}
-        `}
-      >
-        <Link
-          css={css`
-            appearance: none;
-            text-decoration: none;
-            transition: all 0.2s ease-in-out;
-            background-color: transparent;
-            border-radius: 0.35em;
-            border: 3px solid #e1e1e1;
-            color: #e1e1e1;
-            cursor: pointer;
-            display: inline-block;
-            font-weight: 400;
-            line-height: 2.75em;
-            min-width: 7em;
-            max-width: 10em;
-            padding: 0 1.5em;
-            &:hover {
-              background: #2f2d2d;
-              color: #92e5f3;
-              border-color: #92e5f3;
-            }
-            ${mq.sm(css`
-              width: 100%;
-              max-width: -moz-available;
-              max-width: -webkit-fill-available;
-            `)}
-          `}
-          to="/projects"
-        >
-          Projects
-        </Link>
-      </div>
-      <div
-        css={css`
-          ${mq.sm(css`
-            width: 100%;
-            max-width: -moz-available;
-          `)}
-        `}
-      >
-        <Link
-          css={css`
-            appearance: none;
-            text-decoration: none;
-            transition: all 0.2s ease-in-out;
-            background-color: transparent;
-            border-radius: 0.35em;
-            border: 3px solid #e1e1e1;
-            color: #e1e1e1;
-            cursor: pointer;
-            display: inline-block;
-            font-weight: 400;
-            line-height: 2.75em;
-            min-width: 7em;
-            max-width: 10em;
-            padding: 0 1.5em;
-            &:hover {
-              background: #2f2d2d;
-              color: #92e5f3;
-              border-color: #92e5f3;
-            }
-            ${mq.sm(css`
-              width: 100%;
-              max-width: -moz-available;
-              max-width: -webkit-fill-available;
-            `)}
-          `}
-          to="/contact"
-        >
-          Contact
-        </Link>
-      </div>
+      <NavButton title="Home" to="/" />
+      <NavButton title="Projects" to="/projects" />
+      <NavButton title="Contact" to="/contact" />
     </div>
   )
 }
