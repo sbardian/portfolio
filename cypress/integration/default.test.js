ba8cb665588a85c2244d92e85a5f1ccd97320b33
Brian Andrews <sbardian@gmail.com>
2019-03-24 22:13:51 -0600

    Update tests to pass eslint

--- cypress/integration/default.test.js
+++ cypress/integration/default.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 describe("placeholder test", () => {
   it("should pass", () => {
     cy.visit("http://localhost:8000/")
--- cypress/plugins/default.test.js
+++
@@ -1,7 +0,0 @@
-describe("placeholder test", () => {
-  it("should pass", () => {
-    cy.visit("http://localhost:8000/")
-      .getByText("Brian Andrews")
-      .should("exist")
-  })
-})
