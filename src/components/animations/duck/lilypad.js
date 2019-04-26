6182587a2d0a492efab09cb9b1c925d9dc11e549
Brian Andrews <sbardian@gmail.com>
2019-04-26 09:13:39 -0600

    Enable passing props to lilypad

--- src/components/animations/duck/lilypad.js
+++ src/components/animations/duck/lilypad.js
@@ -1,20 +1,30 @@
 import * as THREE from "three"
 
-const { MeshLambertMaterial, Color, CylinderGeometry, Mesh } = THREE
+const { MeshLambertMaterial, CylinderGeometry, Mesh } = THREE
 
-export default () => {
+export default ({ color, radius }) => {
   const lilyGreen = new MeshLambertMaterial({
-    color: new Color("rgb(47, 76, 51)").getHex(),
+    // color: new Color("rgb(47, 76, 51)").getHex(),
+    color,
     flatShading: true,
   })
 
-  const lilyPadGeo = new CylinderGeometry(10, 10, 1, 10, 1, false, 0, 6.1)
+  const lilyPadGeo = new CylinderGeometry(
+    radius,
+    radius,
+    1,
+    10,
+    1,
+    false,
+    0,
+    6.1
+  )
   const lilyPad = new Mesh(lilyPadGeo, lilyGreen)
   lilyPad.castShadow = true
   lilyPad.receiveShadow = true
-  lilyPad.position.x = 30
-  lilyPad.position.y = -10.3
-  lilyPad.rotation.x = 3.14
+  //   lilyPad.position.x = 30
+  //   lilyPad.position.y = -10.3
+  //   lilyPad.rotation.x = 3.14
 
   return {
     lilyPad,
