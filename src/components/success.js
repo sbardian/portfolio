c929672f1f4328882feb790bd55ff99fe01dbfee
Brian Andrews <sbardian@gmail.com>
2019-07-16 14:04:34 -0600

    refactor success component

--- src/components/success.js
+++ src/components/success.js
@@ -1,33 +1,26 @@
-/** @jsx jsx */
 // eslint-disable-next-line
 import React from "react"
-import { jsx, css } from "@emotion/core"
+import { css } from "@emotion/core"
+import styled from "@emotion/styled"
 import mq from "./media-queries"
 
-export default () => {
-  return (
-    <div
-      css={css`
-        display: grid;
-        grid-gap: 20px;
-        grid-template-columns: 1fr;
-        justify-items: center;
-        width: 100%;
-        padding: 40px 0 0 0;
-        ${mq.sm(css`
-          grid-template-columns: 1fr;
-          justify-items: start;
-        `)}
-      `}
-    >
-      <div
-        css={css`
-          margin: 0 20px 0 20px;
-        `}
-      >
-        Thanks, your message has been sent successfully! I will get back to you
-        as soon as possible.
-      </div>
-    </div>
-  )
-}
+const SuccessWrapper = styled.div`
+  display: grid;
+  grid-gap: 20px;
+  grid-template-columns: 1fr;
+  justify-items: center;
+  width: 100%;
+  padding: 40px 0 0 0;
+  margin: 0 20px 0 20px;
+  ${mq.sm(css`
+    grid-template-columns: 1fr;
+    justify-items: start;
+  `)}
+`
+
+export default () => (
+  <SuccessWrapper>
+    Thanks, your message has been sent successfully! I will get back to you as
+    soon as possible.
+  </SuccessWrapper>
+)
