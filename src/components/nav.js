daeb298a81045e25630dfbe28e9d2a4062339699
Brian Andrews <sbardian@gmail.com>
2019-07-16 10:10:51 -0600

    refactor nav

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,30 +1,30 @@
-/** @jsx jsx */
 // eslint-disable-next-line
 import React from "react"
-import { jsx, css } from "@emotion/core"
+import { css } from "@emotion/core"
+import styled from "@emotion/styled"
 import NavButton from "./nav-button"
 import mq from "./media-queries"
 
+const NavWrapper = styled.div`
+  display: grid;
+  grid-gap: 20px;
+  grid-template-columns: repeat(4, 1fr);
+  margin: 0 20px 20px 20px;
+  justify-items: center;
+  padding: 40px 0 0 0;
+  ${mq.sm(css`
+    grid-template-columns: 1fr;
+    justify-items: start;
+  `)}
+`
+
 export default () => {
   return (
-    <div
-      css={css`
-        display: grid;
-        grid-gap: 20px;
-        grid-template-columns: repeat(4, 1fr);
-        margin: 0 20px 20px 20px;
-        justify-items: center;
-        padding: 40px 0 0 0;
-        ${mq.sm(css`
-          grid-template-columns: 1fr;
-          justify-items: start;
-        `)}
-      `}
-    >
+    <NavWrapper>
       <NavButton title="Home" to="/" />
       <NavButton title="Projects" to="/projects" />
       <NavButton title="Animations" to="/animations" />
       <NavButton title="Contact" to="/contact" />
-    </div>
+    </NavWrapper>
   )
 }
