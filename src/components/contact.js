083c58990fb72a752a34f5f8d3b73ddbb20fc664
Brian Andrews <sbardian@gmail.com>
2019-07-16 10:40:09 -0600

    remove contact animation

--- src/components/contact.js
+++ src/components/contact.js
@@ -4,149 +4,136 @@
 import React from "react"
 import { jsx, css } from "@emotion/core"
 import mq from "./media-queries"
-import createAnimation from "./animations/contact/index"
 
-export default () => {
-  React.useEffect(() => {
-    createAnimation()
-  })
-
-  return (
-    <section
+export default () => (
+  <section
+    css={css`
+      margin: 0 20px 20px 20px;
+      ${mq.sm(css`
+        grid-template-columns: 1fr;
+      `)};
+    `}
+  >
+    <h1>Contact me</h1>
+    <p>
+      Feel free to contact me for job opportunities, or any reason you want. If
+      you would rather reach out via email, github or twitter see icons for
+      each. I will get back with you as soon as possible.
+    </p>
+    <form
+      data-testid="test-contact-form"
+      name="contact"
+      method="post"
+      data-netlify="true"
+      netlify-honeypot="youSuckBot"
+      action="/success"
       css={css`
-        margin: 0 20px 20px 20px;
-        ${mq.sm(css`
-          grid-template-columns: 1fr;
-        `)};
+        display: grid;
+        grid-gap: 20px;
+        grid-template-rows: auto 1fr auto;
       `}
     >
-      <h1>Contact me</h1>
-      <p>
-        Feel free to contact me for job opportunities, or any reason you want.
-        If you would rather reach out via email, github or twitter see icons for
-        each. I will get back with you as soon as possible.
-      </p>
-      <canvas
-        css={css`
-          background: transparent;
-        `}
-        id="contact-scene"
-      />
-      <form
-        data-testid="test-contact-form"
-        name="contact"
-        method="post"
-        data-netlify="true"
-        netlify-honeypot="youSuckBot"
-        action="/success"
+      <input type="hidden" name="form-name" value="contact" />
+      <div
         css={css`
           display: grid;
           grid-gap: 20px;
-          grid-template-rows: auto 1fr auto;
+          grid-template-columns: 1fr 1fr;
+          ${mq.sm(css`
+            grid-template-columns: 1fr;
+          `)};
         `}
       >
-        <input type="hidden" name="form-name" value="contact" />
-        <div
+        <div hidden>
+          <label htmlFor="bot-catcher">
+            {`Don’t fill this out if you're human`}
+            <input
+              id="bot-catcher"
+              name="youSuckBot"
+              type="text"
+              aria-label="YouSuckBot"
+            />
+          </label>
+        </div>
+        <input
+          required
+          type="email"
+          name="email"
+          placeholder="Email"
+          aria-label="Email"
           css={css`
-            display: grid;
-            grid-gap: 20px;
-            grid-template-columns: 1fr 1fr;
-            ${mq.sm(css`
-              grid-template-columns: 1fr;
-            `)};
+            border: 2px solid #e1e1e1;
+            transition: all 0.2s ease-in-out;
+            &:focus {
+              border: 2px solid #92e5f3;
+            }
+          `}
+        />
+        <input
+          required
+          type="text"
+          name="name"
+          placeholder="Name"
+          aria-label="Name"
+          css={css`
+            border: 2px solid #e1e1e1;
+            transition: all 0.2s ease-in-out;
+            &:focus {
+              border: 2px solid #92e5f3;
+            }
+          `}
+        />
+      </div>
+      <div
+        css={css`
+          display: grid;
+          grid-gap: 20px;
+          height: 250px;
+        `}
+      >
+        <textarea
+          required
+          name="message"
+          placeholder="Message"
+          aria-label="Message"
+          css={css`
+            border: 2px solid #e1e1e1;
+            transition: all 0.2s ease-in-out;
+            padding: 1em;
+            height: auto;
+            &:focus {
+              border: 2px solid #92e5f3;
+            }
+          `}
+        />
+      </div>
+      <div>
+        <button
+          type="submit"
+          css={css`
+            appearance: none;
+            transition: all 0.2s ease-in-out;
+            background-color: transparent;
+            border-radius: 0.35em;
+            border: 3px solid #e1e1e1;
+            color: #e1e1e1;
+            cursor: pointer;
+            display: inline-block;
+            font-weight: 400;
+            line-height: 2.75em;
+            min-width: 10em;
+            padding: 0 1.5em;
+            margin-bottom: 3em;
+            &:hover {
+              background: #2f2d2d;
+              color: #92e5f3;
+              border-color: #92e5f3;
+            }
           `}
         >
-          <div hidden>
-            <label htmlFor="bot-catcher">
-              {`Don’t fill this out if you're human`}
-              <input
-                id="bot-catcher"
-                name="youSuckBot"
-                type="text"
-                aria-label="YouSuckBot"
-              />
-            </label>
-          </div>
-          <input
-            required
-            type="email"
-            name="email"
-            placeholder="Email"
-            aria-label="Email"
-            css={css`
-              border: 2px solid #e1e1e1;
-              transition: all 0.2s ease-in-out;
-              &:focus {
-                border: 2px solid #92e5f3;
-              }
-            `}
-          />
-          <input
-            required
-            type="text"
-            name="name"
-            placeholder="Name"
-            aria-label="Name"
-            css={css`
-              border: 2px solid #e1e1e1;
-              transition: all 0.2s ease-in-out;
-              &:focus {
-                border: 2px solid #92e5f3;
-              }
-            `}
-          />
-        </div>
-        <div
-          css={css`
-            display: grid;
-            grid-gap: 20px;
-            height: 250px;
-          `}
-        >
-          <textarea
-            required
-            name="message"
-            placeholder="Message"
-            aria-label="Message"
-            css={css`
-              border: 2px solid #e1e1e1;
-              transition: all 0.2s ease-in-out;
-              padding: 1em;
-              height: auto;
-              &:focus {
-                border: 2px solid #92e5f3;
-              }
-            `}
-          />
-        </div>
-        <div>
-          <button
-            type="submit"
-            css={css`
-              appearance: none;
-              transition: all 0.2s ease-in-out;
-              background-color: transparent;
-              border-radius: 0.35em;
-              border: 3px solid #e1e1e1;
-              color: #e1e1e1;
-              cursor: pointer;
-              display: inline-block;
-              font-weight: 400;
-              line-height: 2.75em;
-              min-width: 10em;
-              padding: 0 1.5em;
-              margin-bottom: 3em;
-              &:hover {
-                background: #2f2d2d;
-                color: #92e5f3;
-                border-color: #92e5f3;
-              }
-            `}
-          >
-            Submit
-          </button>
-        </div>
-      </form>
-    </section>
-  )
-}
+          Submit
+        </button>
+      </div>
+    </form>
+  </section>
+)
