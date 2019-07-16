bb6c503b67617b9c280a16638ff37ac789f0953c
Brian Andrews <sbardian@gmail.com>
2019-07-16 11:20:58 -0600

    remove redundant div

--- src/components/nav-button.js
+++ src/components/nav-button.js
@@ -6,43 +6,39 @@ import { Link } from "gatsby"
 import { jsx, css } from "@emotion/core"
 import mq from "./media-queries"
 
-const NavButton = ({ title, to }) => {
-  return (
-    <div>
-      <Link
-        to={to}
-        activeClassName="active-nav"
-        data-testid={`test-${title}-button`}
-        css={css`
-          appearance: none;
-          text-decoration: none;
-          transition: all 0.2s ease-in-out;
-          background-color: transparent;
-          border-radius: 0.35em;
-          border: 3px solid #e1e1e1;
-          color: #e1e1e1;
-          cursor: pointer;
-          display: inline-block;
-          font-weight: 400;
-          line-height: 2.75em;
-          min-width: 7em;
-          max-width: 10em;
-          padding: 0 1.5em;
-          &:hover {
-            background: #2f2d2d;
-            color: #92e5f3;
-            border-color: #92e5f3;
-          }
-          ${mq.sm(css`
-            min-width: 12em;
-          `)}
-        `}
-      >
-        {title}
-      </Link>
-    </div>
-  )
-}
+const NavButton = ({ title, to }) => (
+  <Link
+    to={to}
+    activeClassName="active-nav"
+    data-testid={`test-${title}-button`}
+    css={css`
+      appearance: none;
+      text-decoration: none;
+      transition: all 0.2s ease-in-out;
+      background-color: transparent;
+      border-radius: 0.35em;
+      border: 3px solid #e1e1e1;
+      color: #e1e1e1;
+      cursor: pointer;
+      display: inline-block;
+      font-weight: 400;
+      line-height: 2.75em;
+      min-width: 7em;
+      max-width: 10em;
+      padding: 0 1.5em;
+      &:hover {
+        background: #2f2d2d;
+        color: #92e5f3;
+        border-color: #92e5f3;
+      }
+      ${mq.sm(css`
+        min-width: 12em;
+      `)}
+    `}
+  >
+    {title}
+  </Link>
+)
 
 NavButton.propTypes = {
   title: PropTypes.string.isRequired,
