d9222c220c38804733b14271a56971ac10ba9841
Brian Andrews <sbardian@gmail.com>
2019-07-16 20:11:35 -0600

    add title to technology images

--- src/components/technologies.js
+++ src/components/technologies.js
@@ -15,7 +15,6 @@ import nodejs from "../images/nodejs.svg"
 import npm from "../images/npm.svg"
 import wordpress from "../images/wordpress.svg"
 import android from "../images/android.svg"
-import gitlab from "../images/gitlab.svg"
 import java from "../images/java.svg"
 import jquery from "../images/jquery.svg"
 import nginx from "../images/nginx.svg"
@@ -51,33 +50,32 @@ const TechImage = styled.img`
 
 export default () => (
   <TechWrapper>
-    <TechImage src={react} alt="react" />
-    <TechImage src={redux} alt="redux" />
-    <TechImage src={gatsby} alt="gatsby" />
-    <TechImage src={git} alt="git" />
-    <TechImage src={github} alt="github" />
-    <TechImage src={html5} alt="html5" />
-    <TechImage src={javascript} alt="javascript" />
-    <TechImage src={joomla} alt="joomla" />
-    <TechImage src={sharepoint} alt="sharepoint" />
-    <TechImage src={css3} alt="C#" />
-    <TechImage src={mongodb} alt="mongodb" />
-    <TechImage src={nodejs} alt="nodejs" />
-    <TechImage src={npm} alt="npm" />
-    <TechImage src={wordpress} alt="wordpress" />
-    <TechImage src={android} alt="android" />
-    <TechImage src={gitlab} alt="gitlab" />
-    <TechImage src={java} alt="java" />
-    <TechImage src={jquery} alt="jquery" />
-    <TechImage src={nginx} alt="nginx" />
-    <TechImage src={typescript} alt="typescript" />
-    <TechImage src={visualstudio} alt="visualstudio" />
-    <TechImage src={php} alt="php" />
-    <TechImage src={powershell} alt="powershell" />
-    <TechImage src={cSharp} alt="C#" />
-    <TechImage src={babel} alt="babel" />
-    <TechImage src={webpack} alt="webpack" />
-    <TechImage src={yarn} alt="yarn" />
-    <TechImage src={sequelize} alt="C#" />
+    <TechImage src={react} alt="react" title="React" />
+    <TechImage src={redux} alt="redux" title="Redux" />
+    <TechImage src={gatsby} alt="gatsby" title="Gatsby" />
+    <TechImage src={git} alt="git" title="Git" />
+    <TechImage src={github} alt="github" title="Github" />
+    <TechImage src={html5} alt="html5" title="HTML5" />
+    <TechImage src={javascript} alt="javascript" title="Javascript" />
+    <TechImage src={joomla} alt="joomla" title="Joomla" />
+    <TechImage src={sharepoint} alt="sharepoint" title="SharePoint" />
+    <TechImage src={css3} alt="css3" title="CSS3" />
+    <TechImage src={mongodb} alt="mongodb" title="MongoDB" />
+    <TechImage src={nodejs} alt="nodejs" title="NodeJS" />
+    <TechImage src={npm} alt="npm" title="npm" />
+    <TechImage src={wordpress} alt="wordpress" title="WordPress" />
+    <TechImage src={android} alt="android" title="Android" />
+    <TechImage src={java} alt="java" title="Java" />
+    <TechImage src={jquery} alt="jquery" title="JQuery" />
+    <TechImage src={nginx} alt="nginx" title="nginx" />
+    <TechImage src={typescript} alt="typescript" title="Typescript" />
+    <TechImage src={visualstudio} alt="visualstudio" title="Visual Studio" />
+    <TechImage src={php} alt="php" title="PHP" />
+    <TechImage src={powershell} alt="powershell" title="PowerShell" />
+    <TechImage src={cSharp} alt="C#" title="C#" />
+    <TechImage src={babel} alt="babel" title="Babel" />
+    <TechImage src={webpack} alt="webpack" title="Webpack" />
+    <TechImage src={yarn} alt="yarn" title="Yarn" />
+    <TechImage src={sequelize} alt="sequalize" title="Sequalize" />
   </TechWrapper>
 )
