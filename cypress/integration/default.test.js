ad6e7e3df2c5a15edc0840df72bc611d32c5709f
Brian Andrews <sbardian@gmail.com>
2019-09-04 15:24:58 -0600

    test(cypress): update tests

--- cypress/integration/default.test.js
+++ cypress/integration/default.test.js
@@ -6,27 +6,27 @@ beforeEach(() => {
 
 describe("Home page tests", () => {
   it("Should find my name", () => {
-    cy.getByText("Brian Andrews").should("exist")
+    cy.findByText("Brian Andrews").should("exist")
   })
 
   it("Click Home and confirm welcome message", () => {
-    cy.getByTestId("test-Home-button")
+    cy.findByTestId("test-Home-button")
       .click()
-      .getByText("Welcome to my portfolio site!")
+      .findByText("Welcome to my portfolio site!")
       .should("exist")
   })
 
   it("Click projects and confirm title", () => {
-    cy.getByTestId("test-Projects-button")
+    cy.findByTestId("test-Projects-button")
       .click()
-      .getByText("Recent Projects")
+      .findByText("Recent Projects")
       .should("exist")
   })
 
   it("Click contacts and confirm form is shown", () => {
-    cy.getByTestId("test-Contact-button")
+    cy.findByTestId("test-Contact-button")
       .click()
-      .getByTestId("test-contact-form")
+      .findByTestId("test-contact-form")
       .should("exist")
   })
 })
