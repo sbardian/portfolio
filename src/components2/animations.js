3b4f996aa78dade399d4654fedbb25579148f0ba
Brian Andrews <sbardian@gmail.com>
2019-09-17 21:38:38 -0600

    chore(redesign): add animations component

---
+++ src/components2/animations.js
@@ -0,0 +1,85 @@
+/** @jsx jsx */
+/* eslint-disable jsx-a11y/label-has-associated-control */
+// eslint-disable-next-line
+import React from "react"
+import { jsx, css } from "@emotion/core"
+import { Link } from "gatsby"
+import Article from "./article"
+import duckImage from "../images/duck-image.png"
+import oberynImage from "../images/oberyn-image.png"
+
+const Animations = () => {
+  return (
+    <div
+      css={css`
+        display: flex;
+        flex-direction: column;
+        justify-items: center;
+        align-items: center;
+        margin-left: 20px;
+        margin-right: 20px;
+      `}
+    >
+      <Article type="dark">
+        <p>
+          {`I have started playing with three.js.  Here are a couple of characters I have made so far.  These have been pretty fun to make, so probably some more to come here...`}
+        </p>
+      </Article>
+      <div
+        css={css`
+          display: flex;
+          margin: 10px;
+          border: 1px solid white;
+        `}
+      >
+        <Link
+          css={css`
+            font-size: 24pt;
+            color: white;
+          `}
+          to="/oberynPage"
+        >
+          <img
+            css={css`
+              margin: 0;
+              background-position: center;
+              background-size: cover;
+              width: 100%;
+              height: 450px;
+            `}
+            src={oberynImage}
+            alt="Oberyn"
+          />
+        </Link>
+      </div>
+      <div
+        css={css`
+          margin: 10px;
+          border: 1px solid white;
+        `}
+      >
+        <Link
+          css={css`
+            font-size: 24pt;
+            color: white;
+          `}
+          to="/duckPage"
+        >
+          <img
+            css={css`
+              margin: 0;
+              background-position: center;
+              background-size: cover;
+              width: 100%;
+              height: 450px;
+            `}
+            src={duckImage}
+            alt="Duck"
+          />
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Animations
