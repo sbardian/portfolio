ca44fc83bb6f9aeb3b7f59cbf90536096be790de
Brian Andrews <sbardian@gmail.com>
2019-09-24 19:52:53 -0600

    chore(redesign): stop setting dark as default mode

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -2,4 +2,4 @@ import merge from "deepmerge"
 import colors from "./colors"
 import theme from "./theme"
 
-export default merge({ initialColorMode: `dark`, colors }, theme)
+export default merge({ colors }, theme)
