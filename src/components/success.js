bc7bf29cb20ed935e46cbaf7a31cad717ffef8bb
Brian Andrews <sbardian@gmail.com>
2019-09-26 15:18:25 -0600

    chore(success): remove unused styles from success page

--- src/components/success.js
+++ src/components/success.js
@@ -2,31 +2,11 @@
 // eslint-disable-next-line
 import React from "react"
 import { jsx } from "theme-ui"
-import { css } from "@emotion/core"
 import Article from "./article"
-import mq from "./media-queries"
 
 export default () => (
   <Article type="dark">
-    <p
-      sx={{
-        display: "grid",
-        gridGap: 3,
-        gridTemplateColumns: "1fr",
-        justifyItems: "center",
-        width: "100%",
-        paddingTop: 4,
-        marginLeft: 3,
-        marginRight: 3,
-        fontSize: 1,
-      }}
-      css={css`
-        ${mq.sm(css`
-          grid-template-columns: 1fr;
-          justify-items: start;
-        `)}
-      `}
-    >
+    <p>
       Thanks, your message has been sent successfully! I will get back to you as
       soon as possible.
     </p>
