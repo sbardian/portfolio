e9fec47eabd217ec754b1dd81eff1819af456884
Brian Andrews <sbardian@gmail.com>
2019-12-13 10:04:25 -0700

    refactor: shrink social icons

--- src/components/social.js
+++ src/components/social.js
@@ -8,14 +8,14 @@ const SocialIcons = () => {
   return (
     <div
       sx={{
-        margin: 3,
+        // margin: 3,
         paddingBottom: 3,
         display: "grid",
         gridGap: "20px",
         gridTemplateColumns: "35px 35px 35px",
-        alignItems: "center",
-        justifyItems: "center",
-        justifyContent: "center",
+        // alignItems: "center",
+        justifyItems: "start",
+        // justifyContent: "center",
       }}
     >
       <a
@@ -23,21 +23,21 @@ const SocialIcons = () => {
         sx={{ color: "primary" }}
         aria-label="Github"
       >
-        <FaGithub size="2.5em" />
+        <FaGithub size="1.5em" />
       </a>
       <a
         href="https://twitter.com/xsbardianx"
         sx={{ color: "primary" }}
         aria-label="Twitter"
       >
-        <FaTwitter size="2.5em" />
+        <FaTwitter size="1.5em" />
       </a>
       <a
         href="mailto:sbardian@gmail.com?Subject=Dear Developer..."
         sx={{ color: "primary" }}
         aria-label="Email"
       >
-        <FaEnvelope size="2.5em" />
+        <FaEnvelope size="1.5em" />
       </a>
     </div>
   )
