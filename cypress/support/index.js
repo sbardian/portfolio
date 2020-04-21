eb1d1ab4c5a7382858f0df48af3f45c9ba4cc67c
Brian Andrews <sbardian@gmail.com>
2020-04-21 14:53:36 -0600

    test: remove cypress

--- cypress/support/index.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies */
-
-// Import commands.js using ES2015 syntax:
-import "./commands"
-
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
-import "@testing-library/cypress/add-commands"
---
+++ src/components/avatar.test.js
@@ -0,0 +1,8 @@
+import React from "react"
+import { render } from "@testing-library/react"
+import Avatar from "./avatar"
+
+test("Displays the correct title", () => {
+  const { findByText } = render(<Avatar />)
+  expect(findByText("Brian Andrews")).toBeTruthy()
+})
