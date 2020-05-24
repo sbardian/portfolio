11e11aed9b04e6660b98a9d5c5151253d8435188
Brian Andrews <sbardian@gmail.com>
2020-05-24 13:15:34 -0600

    chore: gut eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     "prettier/react",
     "prettier/@typescript-eslint",
   ],
-  plugins: ["emotion", "react", "prettier", "@typescript-eslint"],
+  plugins: ["@typescript-eslint", "emotion", "react", "prettier"],
   env: {
     jest: true,
     browser: true,
@@ -13,34 +13,4 @@ module.exports = {
     es6: true,
   },
   parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 2018,
-    sourceType: "module",
-  },
-  settings: {
-    "import/resolver": {
-      node: {
-        extensions: [".js", ".jsx", ".ts", ".tsx"],
-      },
-    },
-  },
-  rules: {
-    "import/extensions": [
-      "error",
-      "ignorePackages",
-      {
-        js: "never",
-        mjs: "never",
-        jsx: "never",
-        ts: "never",
-        tsx: "never",
-      },
-    ],
-    "react/jsx-filename-extension": "off",
-    "react/prop-types": "off", // Disable prop-types as we use TypeScript for type checking
-    "@typescript-eslint/explicit-function-return-type": "off",
-  },
 }
