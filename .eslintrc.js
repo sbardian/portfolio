4705603486ff5ac1b0164151cfa564e48af30ae0
Brian Andrews <sbardian@gmail.com>
2020-05-25 13:34:35 -0600

    yRevert "chore: gut eslintrc"
    This reverts commit c1bde34b76123503ffbaad93dd545879775e0d9a.

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,32 @@ module.exports = {
   plugins: ["@typescript-eslint", "emotion", "react", "prettier"],
   env: {
     jest: true,
+    browser: true,
+    node: true,
+    es6: true,
   },
   parser: "@typescript-eslint/parser",
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
+  },
+  rules: {
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        js: "never",
+        mjs: "never",
+        jsx: "never",
+        ts: "never",
+        tsx: "never",
+      },
+    ],
+    "react/jsx-filename-extension": "off",
+    "react/prop-types": "off", // Disable prop-types as we use TypeScript for type checking
+    "@typescript-eslint/explicit-function-return-type": "off",
+  },
 }
