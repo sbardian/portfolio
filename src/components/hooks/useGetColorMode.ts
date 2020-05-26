55eb3f76a38d1a8ea74b63958c08893273f37c8d
Brian Andrews <sbardian@gmail.com>
2020-05-26 00:09:43 -0600

    refactor: account for null return from storage

--- src/components/hooks/useGetColorMode.ts
+++ src/components/hooks/useGetColorMode.ts
@@ -2,14 +2,14 @@ import * as React from "react"
 import { window } from "browser-monads"
 
 interface SetColorMode {
-  setColorMode: React.Dispatch<React.SetStateAction<string | null>>
+  setColorMode: React.Dispatch<React.SetStateAction<string>>
 }
 
 const useGetColorMode = ({ setColorMode }: SetColorMode) => {
   const storage = window.localStorage
 
   React.useEffect(() => {
-    setColorMode(storage.getItem("theme-ui-color-mode"))
+    setColorMode(storage.getItem("theme-ui-color-mode") || "light")
   }, [])
 }
 
