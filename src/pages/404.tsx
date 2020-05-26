c1746e015fe9c042d2f3b34931ef6ea77e4d6b04
Brian Andrews <sbardian@gmail.com>
2020-05-26 00:31:28 -0600

    refactor: type 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,7 +3,7 @@ import * as React from "react"
 import { Styled } from "theme-ui"
 import PageLayout from "../components/page-layout"
 
-const NotFoundPage = () => (
+const NotFoundPage: React.FC = () => (
   <PageLayout>
     <Styled.h1>NOT FOUND</Styled.h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
