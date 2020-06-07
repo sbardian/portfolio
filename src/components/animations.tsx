ff3479786d748ba705c7e45bf46454f42d48da9a
Brian Andrews <sbardian@gmail.com>
2020-06-07 10:46:09 -0600

    feat: use react-fullpage-accordion to list animations

--- src/components/animations.tsx
+++ src/components/animations.tsx
@@ -1,10 +1,42 @@
 /** @jsx jsx */
 /* eslint-disable react/jsx-pascal-case */
 import { jsx, Styled } from "theme-ui"
+import { FullpageAccordion, Panel } from "react-fullpage-accordion"
 import Article from "./article"
 import AnimationImageLink from "./animation-image-link"
 import duckImage from "../images/duck-image.png"
 import oberynImage from "../images/oberyn-image.png"
+
+interface AnimationData {
+  title: string
+  itemId: number
+  top: string
+  middle: string
+  bottom: string
+  imageUrl: string
+  link: string
+}
+
+export const data: Array<AnimationData> = [
+  {
+    title: "Duck Animation",
+    itemId: 1,
+    top: "Give",
+    middle: "Duck",
+    bottom: "a try!",
+    imageUrl: duckImage,
+    link: "/duckPage",
+  },
+  {
+    title: "Oberyn Animation",
+    itemId: 2,
+    top: "Give",
+    middle: "Oberyn",
+    bottom: "a try!",
+    imageUrl: oberynImage,
+    link: "/oberynPage",
+  },
+]
 
 const Animations: React.FC = () => {
   return (
@@ -12,8 +44,9 @@ const Animations: React.FC = () => {
       <Styled.h1>Animations</Styled.h1>
       <div
         sx={{
-          display: "flex",
-          flexDirection: "column",
+          display: "grid",
+          gap: "0.67rem",
+          gridTemplateColumns: "1fr",
           justifyItems: "center",
           alignItems: "center",
         }}
@@ -25,12 +58,28 @@ const Animations: React.FC = () => {
             the best experience.
           </p>
         </Article>
-        <AnimationImageLink
+        <FullpageAccordion height="500px">
+          <Panel itemId="0" background={duckImage}>
+            <p>Give</p>
+            <p>
+              <a href="/duckPage">Duck!</a>
+            </p>
+            <p>a try!</p>
+          </Panel>
+          <Panel itemId="1" background={oberynImage}>
+            <p>Give</p>
+            <p>
+              <a href="/oberynPage">Oberyn!</a>
+            </p>
+            <p>a try!</p>
+          </Panel>
+        </FullpageAccordion>
+        {/* <AnimationImageLink
           imageSrc={oberynImage}
           to="/oberynPage"
           name="Oberyn"
         />
-        <AnimationImageLink imageSrc={duckImage} to="/duckPage" name="Duck" />
+        <AnimationImageLink imageSrc={duckImage} to="/duckPage" name="Duck" /> */}
       </div>
     </div>
   )
